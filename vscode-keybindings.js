eead586b954e1ae0d308f98f1fc706adf45f7eec
Craig Campbell <emtcampbell@outlook.com>
2019-02-08 09:07:31 -0600

    Update vscode-keybindings.js

--- vscode-keybindings.js
+++ vscode-keybindings.js
@@ -5,19 +5,9 @@
         "when": "editorTextFocus && !editorReadonly"
     },
     {
-        "key": "shift+alt+down",
-        "command": "-editor.action.copyLinesDownAction",
-        "when": "editorTextFocus && !editorReadonly"
-    },
-    {
         "key": ".",
         "command": "^acceptSelectedSuggestion",
         "when": "editorTextFocus && suggestWidgetVisible"
-    },
-    {
-        "key": "ctrl+alt+b",
-        "command": "HookyQR.beautify",
-        "when": "editorFocus"
     },
     {
         "key": "alt+insert",
