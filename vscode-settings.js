6d1986d8755bb0a2db25cf52cd45d87f3c8a4639
Craig Campbell <emtcampbell@outlook.com>
2019-02-08 09:18:02 -0600

    Update vscode-settings.js

--- vscode-settings.js
+++ vscode-settings.js
@@ -3,5 +3,6 @@
   "terminal.integrated.shell.windows": "C:\\\\Windows\\\\System32\\\\WindowsPowerShell\\\\v1.0\\\\powershell.exe",
   "htmltagwrap.tag": "div",
   "git.enableSmartCommit": true,
-  "git.confirmSync": false
+  "git.confirmSync": false,
+  "editor.formatOnSave": true
 }
